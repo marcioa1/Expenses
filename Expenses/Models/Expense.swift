5241acc1fe3c1fad04977bc5fd4811a3d04c034d
Marcio Aun Migueis <marcio@ar1mobile.com.br>
2026-08-06 11:26:01 -0300

    Make Expense Identifiable

--- Expenses/Models/Expense.swift
+++ Expenses/Models/Expense.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftData
 
 @Model
-final class Expense {
+final class Expense: Identifiable {
     var id: UUID = UUID()
     var category: Category
     var details: String?
